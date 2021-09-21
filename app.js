3d0453e1701ab4cb3d31141f31e62f9e4b812652
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-09-21 06:24:32 -0700

    Added the data and the functionality is need to commit

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
+// var jsonData = require('./Product Data/Data.json');
 import Data from './Product Data/Data.json';
-require
 
-// console.log("working fine")
+console.log("working fine")
 
 
-// function show() {
-//     // var CURRENT_URL = window.location.href;
-//     var path = window.location.pathname;
-//     var page = path.split("/").pop();
+function show() {
+    var CURRENT_URL = window.location.href;
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    var newUrl = 
+    console.log("Getting url", CURRENT_URL);
+    console.log("Getting pathname", path);
+    console.log("page name is ", page);
+    page = "/hello world"
+    page.join("/yes done")
+    console.log("second page name", page)
+    console.log("this is working from function!!")
+}
+
+LinkManager = () => { }
+
+
+
+async function show() {
+    var data = await fetch(jsonData)
+    console.log(data)
     
-//     // var newUrl = 
-//     // console.log("Getting url", CURRENT_URL.);
-//     console.log("Getting pathname", path);
-//     console.log("page name is ", page);
-//     page = "/hello world"
-//     page.join("/yes done")
-//     console.log("second page name", page)
-//     console.log("this is working from function!!")
-// }
-
-// LinkManager = () => {
-// }
-
-
-
-function show(e) {
-    if(Data['Crunchmaster®|Multi-Grain Crackers|Original Grain Multi-Seed Crackers|2 10 oz Bags'].Prodcuct_Name == e.text){
-
-        console.log("link value", e.text);
-    }
-    else{
-        console.log("not");
+    
+    const show = (e)=> {
+        
+        if (Data['Crunchmaster®|Multi-Grain Crackers|Original Grain Multi-Seed Crackers|2 10 oz Bags'].Prodcuct_Name == e.text) {
+            
+            console.log("link value", e.text);
+        }
+        else {
+            console.log("not");
+        }
     }
 }
+    
+
+
+fetch("./Product Data/Data.json")
+    .then(response => response.json())
+    .then(json => console.log(json))
